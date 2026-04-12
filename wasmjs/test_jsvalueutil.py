37f973bd5351a5e4c9d4f6c7233d41972b0bce4a
Daniel Reed <nmlorg@gmail.com>
2026-04-12 12:26:49 -0700

    Quick cleanup.
    (Adding tests for JSValue.encode would basically duplicate this.)

--- wasmjs/test_jsvalueutil.py
+++ wasmjs/test_jsvalueutil.py
@@ -6,103 +6,43 @@ from wasmjs import jsvalueutil
 from wasmjs import wasmjs
 
 
-def test_jsvalue():  # pylint: disable=too-many-statements
+def test_jsvalue():
     """Test all the [supported] types of JSValues."""
 
     js = wasmjs.WasmJS()
 
-    # pylint: disable=protected-access
+    tag = jsvalueutil.Tag
+    table = [
+        ('1 == 1', tag.BOOL, True),
+        ('1 == 2', tag.BOOL, False),
+        ('null', tag.NULL, None),
+        ('globalThis.__undefined', tag.UNDEFINED, None),
+        ('1 +', tag.EXCEPTION, None),
+        ('"a string".toUpperCase()', tag.STRING, 'A STRING'),
+        ('111 * 2', tag.INT, 222),
+        ('-111 * 3', tag.INT, -333),
+        ('111n * 4n', tag.SHORT_BIG_INT, 444),
+        ('-111n * 5n', tag.SHORT_BIG_INT, -555),
+        ('3 / 2', tag.FLOAT64, 1.5),
+        ('-5 / 2', tag.FLOAT64, -2.5),
+        ('5e-324', tag.FLOAT64, 5e-324),
+        ('-5e-324', tag.FLOAT64, -5e-324),
+        ('2e-324', tag.INT, 0),
+        ('10 ** 100', tag.FLOAT64, 1e100),
+        ('-(10 ** 101)', tag.FLOAT64, -1e101),
+        ('10n ** 102n', tag.BIG_INT, 10**102),
+        ('-(10n ** 103n)', tag.BIG_INT, -10**103),
+        ('1 / 0', tag.FLOAT64, math.inf),
+        ('-1 / 0', tag.FLOAT64, -math.inf),
+        ('Math.sqrt(-4)', tag.FLOAT64, math.nan),
+        ('new Array(1, 2, 3)', tag.OBJECT, [1, 2, 3]),
+    ]
 
-    with js._api.eval_to_jsval('1 == 1') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.BOOL
-        assert jsval.decode() is True
-
-    with js._api.eval_to_jsval('1 == 2') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.BOOL
-        assert jsval.decode() is False
-
-    with js._api.eval_to_jsval('null') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.NULL
-        assert jsval.decode() is None
-
-    with js._api.eval_to_jsval('globalThis.__undefined') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.UNDEFINED
-        assert jsval.decode() is None
-
-    with js._api.eval_to_jsval('1 +') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.EXCEPTION
-        assert jsval.decode() is None
-
-    with js._api.eval_to_jsval('"a string".toUpperCase()') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.STRING
-        assert jsval.decode() == 'A STRING'
-
-    with js._api.eval_to_jsval('111 * 2') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.INT
-        # Since JS_TAG_INT is 0 (and the value is positive), the whole JSValue is just the int.
-        assert jsval.nanbox == 222
-        assert jsval.decode() == 222
-
-    with js._api.eval_to_jsval('-111 * 3') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.INT
-        assert jsval.decode() == -333
-
-    with js._api.eval_to_jsval('111n * 4n') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.SHORT_BIG_INT
-        assert jsval.decode() == 444
-
-    with js._api.eval_to_jsval('-111n * 5n') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.SHORT_BIG_INT
-        assert jsval.decode() == -555
-
-    with js._api.eval_to_jsval('3 / 2') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == 1.5
-
-    with js._api.eval_to_jsval('-5 / 2') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == -2.5
-
-    with js._api.eval_to_jsval('3e-324') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == 3e-324
-
-    with js._api.eval_to_jsval('-3e-324') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == -3e-324
-
-    with js._api.eval_to_jsval('2e-324') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.INT
-        assert jsval.decode() == 0
-
-    with js._api.eval_to_jsval('10 ** 100') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == 1e100
-
-    with js._api.eval_to_jsval('-(10 ** 101)') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == -1e101
-
-    with js._api.eval_to_jsval('10n ** 102n') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.BIG_INT
-        assert jsval.decode() == 10**102
-
-    with js._api.eval_to_jsval('-(10n ** 103n)') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.BIG_INT
-        assert jsval.decode() == -10**103
-
-    with js._api.eval_to_jsval('1 / 0') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == math.inf
-
-    with js._api.eval_to_jsval('-1 / 0') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert jsval.decode() == -math.inf
-
-    with js._api.eval_to_jsval('Math.sqrt(-4)') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.FLOAT64
-        assert math.isnan(jsval.decode())
-
-    with js._api.eval_to_jsval('new Array(1, 2, 3)') as jsval:
-        assert jsval.tag == jsvalueutil.Tag.OBJECT
-        assert jsval.decode() == [1, 2, 3]
+    for js_expr, tag, expected_val in table:
+        with js._api.eval_to_jsval(js_expr) as jsval:  # pylint: disable=protected-access
+            assert jsval.tag == tag
+            actual_val = jsval.decode()
+            if expected_val is math.nan:
+                assert math.isnan(actual_val)
+            else:
+                assert actual_val == expected_val
