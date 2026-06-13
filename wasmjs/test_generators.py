ec16aee59f1b1ec66b4f6a35f6bd8ffc3bbe7f99
Daniel Reed <nmlorg@gmail.com>
2026-06-12 20:10:57 -0700

    Pull generator-specific tests into a separate test_generators.py.

---
+++ wasmjs/test_generators.py
@@ -0,0 +1,157 @@
+"""All generator-specific tests for wasmjs.wasmjs."""
+
+import types
+
+import pytest
+
+from wasmjs import wasmjs
+
+
+def test_generator():
+    """Verify that JS generators can be accessed like Python gens, but behave like JS gens."""
+
+    js = wasmjs.WasmJS()
+    js.eval("""
+      globalThis.gengen = function*(a) {
+        let b = yield `a=${a}`;
+        let c = yield `b=${b}`;
+        let d = yield `c=${c}`;
+        return `d=${d}`;
+      };
+    """)
+
+    # Implicit listification.
+    assert js.eval('Array.from(gengen(5))') == ['a=5', 'b=undefined', 'c=undefined']
+    assert list(js.eval('gengen(5)')) == ['a=5', 'b=null', 'c=null']
+
+    # Manual running.
+    assert js.eval("""
+      let gen = gengen(10);
+      let ret = [];
+      ret.push(gen.next(999));
+      ret.push(gen.next(20));
+      ret.push(gen.next(30));
+      ret.push(gen.next(40));
+      ret.push(gen.next(999));
+      JSON.stringify(ret);
+    """) == """[
+      {"value":"a=10","done":false},
+      {"value":"b=20","done":false},
+      {"value":"c=30","done":false},
+      {"value":"d=40","done":true},
+      {"done":true}
+    ]""".replace(' ', '').replace('\n', '')
+
+    susp = js.eval('gengen(10)')
+    assert isinstance(susp, types.GeneratorType)
+    assert susp.send(None) == 'a=10'
+    assert susp.send(20) == 'b=20'
+    assert susp.send(30) == 'c=30'
+    with pytest.raises(StopIteration, check=lambda e: e.value == 'd=40'):
+        susp.send(40)
+    with pytest.raises(StopIteration, check=lambda e: e.value is None):
+        susp.send(999)
+
+
+def test_generator_edges():
+    """Test a couple edge cases that tripped me up while designing this."""
+
+    js = wasmjs.WasmJS()
+
+    susp = js.eval('function* f() {}; f();')
+    assert isinstance(susp, types.GeneratorType)
+    assert list(susp) == []  # pylint: disable=use-implicit-booleaness-not-comparison
+
+    susp = js.eval('function* f() { yield 1; }; f();')
+    assert isinstance(susp, types.GeneratorType)
+    assert list(susp) == [1]
+
+    susp = js.eval('function* f() { yield 1; yield 2; }; f();')
+    assert isinstance(susp, types.GeneratorType)
+    assert list(susp) == [1, 2]
+
+
+def test_generator_exceptions():
+    """Test exception propagation."""
+
+    js = wasmjs.WasmJS()
+
+    susp = js.eval('function* f() { yield 1; yield bogus; }; f();')
+    assert isinstance(susp, types.GeneratorType)
+    assert next(susp) == 1
+    with pytest.raises(wasmjs.JSError, check=lambda e: e.name == 'ReferenceError'):
+        next(susp)
+    with pytest.raises(StopIteration, check=lambda e: e.value is None):
+        next(susp)
+
+    class _MyExceptionType(Exception):
+        pass
+
+    boom = _MyExceptionType('boom')
+
+    # Handled exceptions do not interrupt generation.
+    susp = js.eval(
+        'function* f() { try { yield 1; } catch (e) { yield `caught ${e}`; } yield 2; }; f();')
+    assert next(susp) == 1
+    assert susp.throw(boom) == 'caught _MyExceptionType: boom'
+    assert next(susp) == 2
+
+    # Uncaught exceptions pass through unmodified.
+    susp = js.eval('function* f() { yield 1; yield 2; }; f();')
+    assert next(susp) == 1
+    with pytest.raises(_MyExceptionType, check=lambda e: e is boom):
+        susp.throw(boom)
+    with pytest.raises(StopIteration):
+        next(susp)
+
+    # Exceptions raised while handling an exception behave the same as exceptions raised under any
+    # other circumstances.
+    susp = js.eval(
+        'function* f() { try { yield 1; } catch (e) { throw `caught ${e}`; } yield 2; }; f();')
+    assert next(susp) == 1
+    with pytest.raises(wasmjs.JSError,
+                       check=lambda e: (e.name, e.message) ==
+                       ('Error', 'caught _MyExceptionType: boom')):
+        susp.throw(boom)
+    with pytest.raises(StopIteration):
+        next(susp)
+
+
+def test_generator_interweave():
+    """Verify concurrent generators behave as expected."""
+
+    js = wasmjs.WasmJS()
+
+    fsusp, gsusp = js.eval("""
+      let count = 0;
+      function* f() { yield ++count; yield ++count; yield ++count; }
+      function* g() { yield ++count; yield ++count; yield ++count; }
+      [f(), g()];
+    """)
+    assert isinstance(fsusp, types.GeneratorType)
+    assert isinstance(gsusp, types.GeneratorType)
+    assert next(gsusp) == 1
+    assert next(gsusp) == 2
+    assert next(fsusp) == 3
+    assert next(gsusp) == 4
+    assert next(fsusp) == 5
+    assert next(fsusp) == 6
+
+    susp = js.eval("""
+      let count = 0;
+      function* f() { yield ++count; yield ++count; yield ++count; }
+      function* g() { yield ++count; yield ++count; yield ++count; }
+      function* h() { yield f(); yield g(); }
+      h();
+    """)
+    assert isinstance(susp, types.GeneratorType)
+    fsusp = next(susp)
+    assert isinstance(fsusp, types.GeneratorType)
+    assert next(fsusp) == 1
+    gsusp = next(susp)
+    assert isinstance(gsusp, types.GeneratorType)
+    assert next(fsusp) == 2
+    assert next(gsusp) == 3
+    assert next(fsusp) == 4
+    assert next(gsusp) == 5
+    assert next(gsusp) == 6
